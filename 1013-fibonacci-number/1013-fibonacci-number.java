f338d55b4f08b2cf4366cd3964c457b28cfd7246
Law Pandit <145872007+lawpandit6@users.noreply.github.com>
2025-09-10 11:17:13 +0530

    Time: 0 ms (100%), Space: 40.3 MB (81.66%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,8 +1,17 @@
 class Solution {
     public int fib(int n) {
-        if(n==0 || n==1){
+        int dp[]=new int[n+1];
+        return fibonacci(n,dp);
+    }
+
+    public int fibonacci(int n, int dp[]){
+        if(n==1 || n==0){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        if(dp[n] !=0){
+            return dp[n];
+        }
+        dp[n]=fibonacci(n-1,dp)+fibonacci(n-2,dp);
+        return dp[n];
     }
 }
